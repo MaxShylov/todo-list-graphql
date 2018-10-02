5958c33b0c87ecdc949a101bb23e350885a9dfad
MaxShylov <shma9013@gmai.com>
2018-10-03 02:09:11 +0300

    make it better

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import { createServer } from "http";
+import path from "path";
 
 import connectFraphQL from "./graphql/connect";
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 const httpServer = createServer(app);
 
-connectFraphQL({app, httpServer});
+connectFraphQL({ app, httpServer });
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 httpServer.listen({ port: PORT }, () => {
   console.log(`Apollo Server on http://localhost:${PORT}/graphql`);
